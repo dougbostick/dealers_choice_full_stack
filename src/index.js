87e200fbd9c4602bb76cd9849cc1444013bd99cc
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2022-03-19 11:46:50 -0400

    working with local state, beginning redux store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
 import React from "react";
 import { render } from "react-dom";
+import axios from "axios";
+import { connect, Provider } from "react-redux";
+import store from "./store";
 
-render(<hr />, document.querySelector("#root"));
+class CitiesList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cities: [],
+    };
+  }
+  async componentDidMount() {
+    const response = await axios.get("/api/cities");
+    const data = response.data;
+    // console.log("data", data);
+    this.setState({ cities: data });
+  }
+  render() {
+    console.log("state", this.state);
+    const citiesEls = this.state.cities.map((city) => {
+      return <li key={city.id}>{city.name}</li>;
+    });
+    return (
+      <div>
+        <h1>Cities</h1>
+        <ul>{citiesEls}</ul>
+      </div>
+    );
+  }
+}
+const ConnectedCities = connect((state) => state, null)(CitiesList);
+render(
+  <Provider store={store}>
+    <ConnectedCities />
+  </Provider>,
+  document.querySelector("#root")
+);
---
+++ src/store.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import axios from "axios";
+
+export const getCities = () => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get("/api/cities");
+      const data = response.data;
+      store.dispatch({ type: "CITIES", cities: data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+const initialState = { cities: [] };
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "CITIES": {
+      state = { cities: action.cities };
+    }
+  }
+};
+const store = createStore(reducer, applyMiddleware(thunk));
+
+export default store;
